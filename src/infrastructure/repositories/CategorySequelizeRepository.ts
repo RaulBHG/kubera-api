a3306d31a9725e24e5274be3f71e0c7b51d56e9b
RaulBHG <raulblazquezhernangomez@gmail.com>
2024-12-19 19:22:12 +0100

    git add .

--- src/infrastructure/repositories/CategorySequelizeRepository.ts
+++ src/infrastructure/repositories/CategorySequelizeRepository.ts
@@ -5,7 +5,11 @@ const CategoryModel = require("../../../models").category;
 
 export class CategorySequelizeRepository implements CategoryRepositoryContract {
   async getAll(): Promise<Category[]> {
-    const categories = await CategoryModel.findAll();
+    const categories = await CategoryModel.findAll({
+      where: {
+        visible: true,
+      },
+    });
     return categories.map(
       (category: typeof CategoryModel) =>
         new Category(
